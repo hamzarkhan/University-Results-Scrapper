1db1ecab495d156953b3f11dbffac833c64f4a48
that-ar-guy <mohdarahman124@gmail.com>
2024-03-10 20:19:25 +0530

    layout changed

--- resultapp/forms.py
+++ resultapp/forms.py
@@ -2,8 +2,8 @@ from django import forms
 
 class ResultForm(forms.Form):
     result_link = forms.URLField(label='Result Link', required=True)
-    college_code = forms.ChoiceField(label='College Code', choices=[('1604', 'MJCET'), ('1605', 'ISL'), ('1610', 'NSAKCET')], required=True)
-    field_code = forms.ChoiceField(label='Field Code', choices=[('748','AIML'),('749','IOT'),('733','CSE')], required=True)
+    college_code = forms.ChoiceField(label='College Code', choices=[ ('1604', 'MJCET'), ('1605', 'ISL'), ('1610', 'NSAKCET'),('2455','KMEC'),('2453','NGIT')], required=True)
+    field_code = forms.ChoiceField(label='Field Code', choices=[('748','AIML'),('749','IOT'),('750','DS'),('736','MECH'),('733','CSE'),('732','CIVIL')], required=True)
     
     # Changed from ChoiceField to CharField for year
     year = forms.ChoiceField(label='Year', choices=[('20', '4th year'), ('21', '3rd year'), ('22', '2nd year'), ('23', '1st year')], required=True)
