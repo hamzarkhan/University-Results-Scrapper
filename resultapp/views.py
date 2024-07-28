fb6a702cf6f62191b2377ab12dcb65f0a0e146e9
that-ar-guy <mohdarahman124@gmail.com>
2024-07-28 16:29:56 +0530

    the downloaded file name settled

--- resultapp/views.py
+++ resultapp/views.py
@@ -109,7 +109,7 @@ def extract_subjects_with_f_grade(soup):
         sub_code = columns[0].text.strip()
         sub_name = columns[1].text.strip()
         grade = columns[4].text.strip()
-        if grade == 'F':
+        if grade == 'F' or grade =='Ab':
             f_grade_subjects.append(f"{sub_code} - {sub_name}")
     return f_grade_subjects
 
@@ -119,7 +119,7 @@ def is_valid_combination(college_code, field_code):
         '1604': ['748','735','734','737', '750', '736', '733', '732'],  # MJCET
         '1603':['735','736','737','732','734','733'], # DECCAN
         '1605': ['736', '733', '732'],          # ISL
-        '1610': ['748', '749', '750', '736', '733', '732'],  # NSAKCET
+        '1610': ['748', '749', '750', '736', '733', '732','737'],  # NSAKCET
         '2455': ['748', '733',],          # KMEC
         '2453': ['748','733']   # NGIT
     }
