d2a9e1f95efe19ccc38436b8733fa4540aae4338
that-ar-guy <mohdarahman124@gmail.com>
2024-07-28 22:53:49 +0530

    second try

--- UnversityResults/settings.py
+++ UnversityResults/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-a+j5d(-^o(^a*&=lw4#*_^lswyy^_s2_57$)+9a&4_v$lc#ii*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app','.now.sh','localhost','http://127.0.0.1']
 
 
 # Application definit
@@ -122,3 +122,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS=os.path.join(BASE_DIR,'static')
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles_build','static')
